1d3eead9c81407c1d38110d694d92141b8af346f
CSJasper <jasper695@icloud.com>
2023-11-03 11:59:30 +0900

    updates

--- clean_t2v.sh
+++ clean_t2v.sh
@@ -5,3 +5,8 @@ rm -rf *
 mv ../make_video.py ./
 mv ../motion2skeleton ./
 cd ../
+cd ./text2motion
+cp ./text2motion.py ../text2motion.py
+rm -rf *
+mv ../text2motion.py ./
+cd ../
--- prepare_t2v.sh
+++ prepare_t2v.sh
@@ -1,3 +1,10 @@
+cd ./text2motion
+git clone https://github.com/Mael-zys/T2M-GPT.git
+cd ./T2M-GPT
+cp -r * ../
+cd ../
+rm -rf ./T2M-GPT
+cd ../
 cd ./text2video
 git clone https://github.com/Picsart-AI-Research/Text2Video-Zero.git
 cd ./Text2Video-Zero
